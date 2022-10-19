5cb4d833d219da271d40a641b76b495cec3737f8
Harish Vadgama <harishvadgama@outlook.com>
2022-10-19 21:12:15 +0100

    Adds comment

--- milestone_4.py
+++ milestone_4.py
@@ -1,16 +1,6 @@
 
 import random
 
-
-
-
-'''
-Make sure you complete all the TODOs in this file.
-The prints have to contain the same text as indicated, don't add any more prints,
-or you will get 0 for this assignment.
-'''
-import random
-
 class Hangman:
     '''
     A Hangman Game that asks the user for a letter and checks if it is in the word.
@@ -49,18 +39,18 @@ class Hangman:
     
     
     
-    list_of_guesses=[]
+    
     def __init__(self, word_list, num_lives=5):
         # TODO 2: Initialize the attributes as indicated in the docstring
         # TODO 2: Print two message upon initialization:
         # 1. "The mistery word has {num_letters} characters"
         # 2. {word_guessed}
-        self.word_list=word_list
-        self.num_lives= num_lives
-        self.word = random.choice(self.word_list)
-        self.word_guessed=['_']*len(self.word)
-        self.list_of_guesses=[]
-        self.num_letters=len(self.word)
+        self.word_list=word_list#list of fruits
+        self.num_lives= num_lives#number of lives
+        self.word = random.choice(self.word_list)#random word chosen from list of words
+        self.word_guessed=['_']*len(self.word)#list of the correct word which will be filled with correct guesses
+        self.list_of_guesses=[]#the list of guesses which the guesses will be appended to
+        self.num_letters=len(set(self.word))#number of letters still left to guess 
     
 
     def check_guess(self, guess):
@@ -80,17 +70,19 @@ class Hangman:
         # TODO 3: If the letter is in the word, the number of UNIQUE letters in the word that have not been guessed yet has to be reduced by 1
         # TODO 3: If the letter is not in the word, reduce the number of lives by 1
         # Be careful! A letter can contain the same letter more than once. TIP: Take a look at the index() method in the string class
-        if guess.lower() in self.word:
-            print(f"Good guess! {guess.lower()} is in the word.")
+        guess=guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
             for i in range(len(self.word)):
-                if self.word[i]==guess.lower():
-                    self.word_guessed[i]=guess.lower()
+                if self.word[i]==guess:
+                    self.word_guessed[i]=guess
             self.num_letters-=1
         else:
             self.num_lives-=1
-            print(f"Sorry {guess.lower()} is not in the word. Try again.")
-            print(f"You have {self.num_lives} lives left")
-        self.list_of_guesses.append(guess.lower())
+            print(f"Sorry {guess} is not in the word. Try again.")
+            print(f"You have {self.num_lives} lives left")     
+        self.list_of_guesses.append(guess) 
+
 
     def ask_for_input(self):
         '''
@@ -106,35 +98,38 @@ class Hangman:
         # TODO 3: If the letter is valid, call the check_letter method
         
         
-        while True:
-            guess =input("enter a single letter")
-            guess=guess.lower()
-            if len(guess)!=1 or not guess.isalpha():
-                print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in self.list_of_guesses:
-                print("You already tried that letter!")
-            else:
-                self.check_guess(guess)
+        guess =input("enter a single letter")
+        guess=guess.lower()
+        if len(guess)!=1 or not guess.isalpha():
+            print("Invalid letter. Please, enter a single alphabetical character.")
+        elif guess in self.list_of_guesses:
+            print("You already tried that letter!")
+        else:
+            self.check_guess(guess)
             self.list_of_guesses.append(guess)
+       
+                
 
-#def play_game(word_list):
+# In the play game definition I call the hangman class and create in instance called game. 
+#using a while loop I set the conditions for winning and losing. Aswell as continuing the game 
+#is its still in progress.
+
+def play_game():
     # As an aid, part of the code is already provided:
- #   game = Hangman(word_list, num_lives=5)
-    # TODO 1: To test this task, you can call the ask_letter method
-    # TODO 2: To test this task, upon initialization, two messages should be printed 
-    # TODO 3: To test this task, you call the ask_letter method and check if the letter is in the word
+    game = Hangman(word_list, num_lives=5)
+    while True:
+        if game.num_lives==0:
+            print(f'You lost! The word was {game.word}')
+            break
+        elif game.num_letters>0:
+            game.ask_for_input()
+
+        elif game.num_lives!=0 and game.num_letters==0:
+            print('Congratulations!you won!')
+            break
+         
+
+
+word_list = ["apple", "mango", "peach", "blueberry", "watermelon"]
+play_game()
     
-    # TODO 4: Iteratively ask the user for a letter until the user guesses the word or runs out of lives
-    # If the user guesses the word, print "Congratulations! You won!"
-    # If the user runs out of lives, print "You lost! The word was {word}"
-
-  #  pass
-
-if __name__ == '__main__':
-    word_list=['banana', 'apple', 'blueberry', 'mango', 'peach']
-    
-    #play_game(word_list)
-
-checking=Hangman(word_list)
-print(checking.ask_for_input())
-print(checking.word)
